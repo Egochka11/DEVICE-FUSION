20df879bc356d394d9a7b8e30a4698ab8e9bc969
Egochka11 <t.novgorodtseva77@gmail.com>
2025-08-31 20:12:17 +0700

    commit 1 - add a lot of things, still messing around

---
+++ GamemakerModMerger/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using GamemakerModMerger;
+using System.IO;
+using System.Reflection;
+using UndertaleModLib;
+string programLocation = Directory.GetParent(Assembly.GetExecutingAssembly().Location).ToString();
+//Console.WriteLine(programLocation);
+
+Console.WriteLine("give me a path to a data.win file");
+var datapath = Console.In.ReadLine();
+Console.WriteLine("give me a path to a xdelta file");
+var xdeltapath = Console.In.ReadLine();
+UndertaleData data;
+try
+{
+    using var stream = new FileStream(datapath, FileMode.Open, FileAccess.Read);
+    data = UndertaleIO.Read(stream, (warning, _) => { Console.WriteLine("A warning occured while trying to load " + datapath + ": " + warning); Console.Error.Close(); });
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.ToString());
+    return;
+}
+Directory.CreateDirectory("cache");
+Console.WriteLine("string index 100 is " + data.Strings[100].ToString());
+File.WriteAllText("cache\\testfile.txt", "lol");
+Console.ForegroundColor = ConsoleColor.Cyan;
+
+await xdelta_patches.PatchXdelta(datapath, xdeltapath, programLocation + "\\cache\\data.win");
---
+++ GamemakerModMerger/xdelta patches.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamemakerModMerger
+{
+    internal static class xdelta_patches
+    {
+        public static async Task PatchXdelta(string data, string patch, string output)
+        {
+            using (var process = new Process())
+            {
+                if (OperatingSystem.IsWindows())
+                {
+                    process.StartInfo.FileName = "xdelta3-3.1.0-x86_64.exe";
+                    process.StartInfo.Arguments = $"-d -f -s \"{data}\" \"{patch}\" \"{output}\"";
+                }
+                process.StartInfo.CreateNoWindow = false;
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.RedirectStandardOutput = true;
+                process.Start();
+
+                await process.WaitForExitAsync();
+            }
+        }
+    }
+}
